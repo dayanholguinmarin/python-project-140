f613e0b8ce5f789635c36940700bb8d9db516f9c
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2025-11-10 21:18:34 -0500

    correccion name even
    even correcion

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,11 +6,11 @@ def random_number():
   return randint(1, 100)
 
 
-def first_question():
+def first_question(name):
     print('Answer "yes" if the number is even, otherwise answer "no".')
     
     for i in range(1, 4):  # Repite 3 veces
-        number = random_number()  #aqui ya llame esta funcion que hice arriba
+        number = random_number()  # aqui ya llame esta funcion que hice arriba
         is_even = number % 2 == 0 # aqui se si es par o impar el divisible
         VALID_ANSWER = ['yes', 'no']
         correct_answer = 'yes' if is_even else 'no'
